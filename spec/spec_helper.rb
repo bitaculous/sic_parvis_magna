2365812746328b848af9713893836a1a31df46cd
Maik Kempe <mkempe@nrolla.com>
2013-09-10 12:13:17 +0200

    [♻] Cleaned up.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
 
-lib = File.expand_path('../../lib', __FILE__)
+lib = File.expand_path '../../lib', __FILE__
 $:.unshift lib unless $:.include? lib
 
 require 'sic_parvis_magna'
