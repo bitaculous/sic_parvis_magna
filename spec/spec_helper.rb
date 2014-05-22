31bfc9b9549222dcf7d5ba8f4c47a4585196dd8d
Maik Kempe <mkempe@nrolla.com>
2014-05-22 13:15:19 +0200

    [➠] Updated spec helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,5 +6,12 @@ $:.unshift lib unless $:.include? lib
 require 'sic_parvis_magna'
 
 RSpec.configure do |config|
-  # ...
+  config.expect_with :rspec do |rspec|
+    rspec.syntax = :expect # Disable the “should” and use the “expect” syntax
+  end
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  config.order = 'random'
 end
